274081c2b0af10febfc764523f9fd8db9914b68f
vince-exe <vincenzocaliendoantonio@gmail.com>
2022-11-06 01:18:36 +0100

    update
    updated the todo.html page

--- WebServer/routers/toDoListRouter.js
+++ WebServer/routers/toDoListRouter.js
@@ -4,7 +4,7 @@ const router = express.Router()
 const toDoListController = require('../controllers/toDoListController')
 const AUTHMiddleware = require('../middlewares/AUTHMiddleware')
 
-router.route('/').post(AUTHMiddleware.checkAUTH, (req, resp) => { return resp.sendStatus(200) })
+router.route('/').post(AUTHMiddleware.checkAUTH, (req, resp) => { resp.sendStatus(200) })
 
 router.route('/api/get-todo').post(AUTHMiddleware.checkAUTH, toDoListController.getToDoList)
 
